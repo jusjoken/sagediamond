c926449d0e8a14dddfa0df52f9c661263d1ac41c
skyebanta <SKYEBANTA@GMAIL.COM>
2011-05-06 12:29:20 +0000

    API change for new release

--- SageDiamond/src/sagediamond/api.java
+++ SageDiamond/src/sagediamond/api.java
@@ -14,7 +14,7 @@ import sagex.UIContext;
 public class api {
 
 
-    public static String Version = "3.27";
+    public static String Version = "3.30";
 
 
     public static int GetSeasonEpisodeNumber(Object MediaObject) {
