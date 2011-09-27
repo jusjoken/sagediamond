63c99a3029426953e3322bcda30c0660e349d16a
usjusjoken <usjusjoken@gmail.com>
2011-09-27 20:10:03 +0000

    Just testing the code - implemented log4j setup

--- SageDiamond/src/SDGroup/PloxeeAPI.java
+++ SageDiamond/src/SDGroup/PloxeeAPI.java
@@ -6,9 +6,6 @@
 package SDGroup;
 
 
-import java.util.HashMap;
-
-import sun.rmi.runtime.Log;
 import org.apache.log4j.Logger;
 import org.apache.log4j.PropertyConfigurator;
 /**
@@ -19,22 +16,29 @@ import org.apache.log4j.PropertyConfigurator;
  */
 public class PloxeeAPI {
 
-    public static Logger LOG=null;
+    //public static Logger LOG=null;
+    static private final Logger LOG = Logger.getLogger(PloxeeAPI.class);
 
-    public static void Load(String PropertyAdder){
-        //    Log4jConfigurator.configureQuietly("ploxeetv", this.getClass().getClassLoader());
-        LOG = Logger.getLogger(PloxeeAPI.class);
-        PropertyConfigurator.configure("plugins/SageDiamond/SageDiamond.log4j.properties");
-
-        LOG.info("Logger created successfully!");
-        SortMethods.PropertyAdder=PropertyAdder;
-        SortMethods.PropertyPrefix=SortMethods.PropertyDefault+PropertyAdder+"/";
-        //    LOG.info("Loading Theme");
-        //    ThemeProperties.LoadThemeProperties();
-        //    Theme.SetTheme();
-        //    LOG.info("Done loading Theme");
-        //
-   }
+//    public static void Load(String PropertyAdder){
+//        //    Log4jConfigurator.configureQuietly("ploxeetv", this.getClass().getClassLoader());
+//        //LOG = Logger.getLogger(PloxeeAPI.class);
+//        // original //PropertyConfigurator.configure("plugins/SageDiamond/SageDiamond.log4j.properties");
+//        PropertyConfigurator.configure("STVs/SageDiamond/Configuration/SageDiamond.log4j.properties");
+//
+//        LOG.info("Logger created successfully!");
+//        LOG.debug("Test Log Message - debug");
+//        LOG.info("Test Log Message - info");
+//        LOG.warn("Test Log Message - warn");
+//        LOG.error("Test Log Message - error");
+//        LOG.fatal("Test Log Message - fatal");
+//        SortMethods.PropertyAdder=PropertyAdder;
+//        SortMethods.PropertyPrefix=SortMethods.PropertyDefault+PropertyAdder+"/";
+//        //    LOG.info("Loading Theme");
+//        //    ThemeProperties.LoadThemeProperties();
+//        //    Theme.SetTheme();
+//        //    LOG.info("Done loading Theme");
+//        //
+//   }
 
     public static void LoadView(String CurrView){
         SortMethods.PropertyPrefix=SortMethods.PropertyDefault+CurrView+"/";
@@ -63,22 +67,28 @@ public class PloxeeAPI {
     }
 
     public static void TestMe(String PropertyAdder,String Method){
+        LOG.info("TestMe: started");
         Object[] Test=null;
         if(Method.equals("GettingFiles")){
+            LOG.info("TestMe: ByTitle");
             Test=(Object[]) Groups.GetAllVidsByTitle(PropertyAdder, Method,null,null);
         }
         else{
+            LOG.info("TestMe: ByGroup");
             Test = (Object[]) GroupBuilder.GetAllVideosGrouped(PropertyAdder, Method);
         }
 
         for(Object Curr:Test){
+            LOG.info("TestMe: looping");
             System.out.println("TestMe: Curr="+sagex.api.ShowAPI.GetShowTitle(Curr));
         }
         System.out.println("TestMe: Test Size"+Test.length);
+        LOG.info("TestMe: Test Size"+Test.length);
 
         Object tester =sagex.api.Database.GroupByArrayMethod(Test,"sagediamond_MetadataCalls_GetMediaTitle");
 
         System.out.println("TestMe: " + tester.getClass() + " - done");
+        LOG.info("TestMe: " + tester.getClass() + " - done");
 
     }
     
