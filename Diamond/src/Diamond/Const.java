b198d1225fea183c874e62c1b8d4b5a1f875d4c1
usjusjoken <usjusjoken@gmail.com>
2011-10-30 01:10:45 +0000

    Working on flow Options for sorting - version 4.002 in progress

--- Diamond/src/Diamond/Const.java
+++ Diamond/src/Diamond/Const.java
@@ -26,6 +26,7 @@ public class Const {
     public static final String OptionsIteration = "OptionsIteration";
     public static final String FlowName = "FlowName";
     public static final String FlowType = "FlowType";
+    public static final String FlowSort = "FlowSort";
     public static final String FlowPathFilters = "FlowPathFilters";
     public static final String FlowNameNotFound = "Flow Name Not Found";
     public static final String FlowTypeDefault = "Cover Flow";
--- Diamond/src/Diamond/api.java
+++ Diamond/src/Diamond/api.java
@@ -23,7 +23,7 @@ public class api {
 
     public static Logger LOG=null;
 
-    public static String Version = "4.001";
+    public static String Version = "4.002";
 
     public static PropList YESNOList = new PropList();
     public static PropList ONOFFList = new PropList();
