4c4cf9f3dc2516b973611b1780d9f330eb8e0e5b
usjusjoken@gmail.com <usjusjoken@gmail.com@91caa8ea-7dc4-6ad5-af74-5b2e97dbf199>
2011-12-27 00:00:44 +0000

    LoadView in Source - groups/filters/sorts - in progress  - version 4.011

--- trunk/Diamond/src/Diamond/SourceUI.java
+++ trunk/Diamond/src/Diamond/SourceUI.java
@@ -4,14 +4,11 @@
  */
 package Diamond;
 
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedHashSet;
 import java.util.SortedMap;
 import java.util.TreeMap;
 import org.apache.log4j.Logger;
-import sagex.phoenix.factory.ConfigurableOption.ListValue;
-import sagex.phoenix.factory.IConfigurable;
 
 /**
  *
@@ -20,10 +17,10 @@ import sagex.phoenix.factory.IConfigurable;
 public class SourceUI {
     static private final Logger LOG = Logger.getLogger(SourceUI.class);
     private String thisFlowName = "";
-    private Boolean thisHasPresentation = Boolean.FALSE;
     private Integer thisLevels = 0;
     private SortedMap<Integer,PresentationUI> thisUIList = new TreeMap<Integer,PresentationUI>();
     public static enum OrganizerType{GROUP,SORT};
+    public static final String OptionNotSet = "NotSet";
     
     public SourceUI(String FlowName){
         this.thisFlowName = FlowName;
@@ -43,7 +40,6 @@ public class SourceUI {
             }
         } while (tUI.HasContent());
         thisLevels = thisUIList.size();
-        thisHasPresentation = !thisUIList.isEmpty();
         LOG.debug(LogMessage());
     }
     
@@ -58,16 +54,23 @@ public class SourceUI {
     }
     public String IsFlat(){
         String tProp = Const.FlowSourceUI + Const.PropDivider + "IsFlat";
-        return Flow.GetOptionName(thisFlowName, tProp, Boolean.TRUE.toString());
+        return Flow.GetOptionName(thisFlowName, tProp, OptionNotSet);
     }
     public String PruneSingleItemFolders(){
         String tProp = Const.FlowSourceUI + Const.PropDivider + "PruneSingleItemFolders";
-        return Flow.GetOptionName(thisFlowName, tProp, Boolean.TRUE.toString());
+        return Flow.GetOptionName(thisFlowName, tProp, OptionNotSet);
     }
     
     //Presentation specific settings
     public Boolean HasPresentation(){
-        return thisHasPresentation;
+        if (!thisUIList.isEmpty()){
+            return Boolean.TRUE;
+        }else{
+            if (IsSet(IsFlat()) || IsSet(PruneSingleItemFolders())){
+                return Boolean.TRUE;
+            }
+        }
+        return Boolean.FALSE;
     }
     public void ClearPresentation(){
         String tProp = Flow.GetFlowBaseProp(thisFlowName) + Const.PropDivider + Const.FlowSourceUI;
@@ -104,136 +107,12 @@ public class SourceUI {
         String tProp = GetPresentationProp(Level) + OrgType + Const.PropDivider + Option;
         Flow.SetOption(FlowName, tProp, NewValue);
     }
-    //Expose UI functions to Sage
-    public static Integer GetUILevel(PresentationUI inUI){
-        return inUI.Level();
+    public static Boolean IsSet(String Value){
+        if (Value.equals(OptionNotSet)){
+            return Boolean.FALSE;
+        }else{
+            return Boolean.TRUE;
+        }
     }
-    
-//    public class UI{
-//
-//        private Organizer thisGroupBy = null;
-//        private Organizer thisSortBy = null;
-//        private Integer thisLevel = 0;
-//        //HasContent is set to true if properties are found for this UI
-//        private Boolean HasContent = Boolean.FALSE;
-//        
-//        public UI(String FlowName, Integer Level){
-//            this.thisLevel = Level;
-//            this.thisGroupBy = new Organizer(FlowName,Level,OrganizerType.GROUP);
-//            this.thisSortBy = new Organizer(FlowName,Level,OrganizerType.SORT);
-//            if (thisGroupBy.HasContent() || thisSortBy.HasContent()){
-//                HasContent = Boolean.TRUE;
-//            }
-//        }
-//        public Boolean HasContent(){
-//            return HasContent;
-//        }
-//        public Organizer Group(){
-//            return this.thisGroupBy;
-//        }
-//        public Organizer Sort(){
-//            return this.thisSortBy;
-//        }
-//        public Integer Level(){
-//            return this.thisLevel;
-//        }
-//        public String LogMessage(){
-//            String tMess = "Level-";
-//            tMess = tMess + thisLevel + "|" + thisGroupBy.LogMessage() + "|" + thisSortBy.LogMessage();
-//            return tMess;
-//        }
-//
-//        public class Organizer{
-//
-//            private String Name = "";
-//            private String optIgnoreAll = "";
-//            private String optIgnoreThe = "";
-//            private String optEmptyFoldername = "";
-//            private String optPruneSingleItemGroups = "";
-//            private HashMap<String,String> optList = new HashMap<String, String>();
-//            private OrganizerType thisType = null;
-//            //HasContent is set to true if properties are found for this organizer
-//            private Boolean HasContent = Boolean.FALSE;
-//
-//            public Organizer(String FlowName, Integer Level, OrganizerType Type){
-//                thisType = Type;
-//                IConfigurable tOrganizer = null;
-//                String tName = GetOrgValue(FlowName, Type.toString(), Level, "Name");
-//                if (!tName.equals(util.OptionNotFound)){
-//                    this.Name = tName;
-//                    this.HasContent = Boolean.TRUE;
-//                    if (Type.equals(OrganizerType.GROUP)){
-//                        tOrganizer = phoenix.umb.CreateGrouper(this.Name);
-//                    }else{
-//                        tOrganizer = phoenix.umb.CreateSorter(this.Name);
-//                    }
-//                    //LOG.debug(myType() + ": '" + this.Name + "' OptionsList '" + tOrganizer.getOptionNames() + "'");
-//                    for (String tOpt: tOrganizer.getOptionNames()){
-//                        String tValue = GetOrgValue(FlowName, Type.toString(), Level, tOpt);
-//                        if (!tValue.equals(util.OptionNotFound)){
-//                            optList.put(tOpt, tValue);
-//                        }
-//                        
-//                    }
-//                }
-////                if (Type.equals(OrganizerType.GROUP)){
-////                    if (Level==0){
-////                        this.Name = "show";
-////                        this.HasContent = Boolean.TRUE;
-////                        optList.put("empty-foldername", "EMPTY");
-////                        optList.put("prune-single-item-groups", "true");
-////                    }else if (Level==1){
-////                        this.Name = "season";
-////                        this.HasContent = Boolean.TRUE;
-////                    }
-////                    if (this.HasContent){
-////                        tOrganizer = phoenix.umb.CreateGrouper(this.Name);
-////                    }
-////                }else{
-////                    if (Level==0 || Level==1){
-////                        this.Name = "title";
-////                        this.HasContent = Boolean.TRUE;
-////                        optList.put("ignore-all", "true");
-////                    }
-////                    if (this.HasContent){
-////                        tOrganizer = phoenix.umb.CreateSorter(this.Name);
-////                    }
-////                }
-//                if (this.HasContent){
-//                    LOG.debug(myType() + ": '" + this.Name + "' OptionsList '" + tOrganizer.getOptionNames() + "'");
-//                    for (String tOpt: tOrganizer.getOptionNames()){
-//                        if (tOrganizer.getOption(tOpt).isList()){
-//                            for (ListValue Item: tOrganizer.getOption(tOpt).getListValues()){
-//                                LOG.debug(myType() + ": Option '" + tOpt + "' Name '" + Item.getName() + "' Value '" + Item.getValue() + "'");
-//                            }
-//                        }else{
-//                            LOG.debug(myType() + ": Option '" + tOpt + "' Not a list");
-//                        }
-//                    }
-//                }
-//            }
-//            public Boolean HasContent(){
-//                return HasContent;
-//            }
-//            public String Name(){
-//                return this.Name;
-//            }
-//            public String myType(){
-//                return thisType.toString();
-//            }
-//            public HashMap<String,String> optList(){
-//                return optList;
-//            }
-//            public String LogMessage(){
-//                String tMess = myType() + "-";
-//                tMess = tMess + Name;
-//                for (String tOpt:optList().keySet()){
-//                    tMess = tMess + ":" + tOpt + "=" + optList.get(tOpt);
-//                }
-//                return tMess;
-//            }
-//        }
-//
-//    }
 
 }
