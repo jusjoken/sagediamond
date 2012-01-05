23112cb2104e7489214baef689aa282021de3a30
usjusjoken <usjusjoken@gmail.com>
2012-01-05 19:53:17 +0000

    Fixing some filters etc - version 4.012

--- Diamond/src/Diamond/Const.java
+++ Diamond/src/Diamond/Const.java
@@ -55,5 +55,6 @@ public class Const {
     public static final String UnknownName = "-UnknownName-";
     public static final String BaseSource = "gemstone.source.Base";
     public static final String PropertyComment = "---gemstone Export Properties - Do Not Manually Edit---";
+    public static final String WrappedFilter = "WrappedFilter";
     
 }
