e614fc3ef24f49345ca024fa22115d5a3544e042
usjusjoken@gmail.com <usjusjoken@gmail.com@91caa8ea-7dc4-6ad5-af74-5b2e97dbf199>
2012-02-07 16:08:48 +0000

    Cache work in progress - version 4.013

--- trunk/Diamond/src/Diamond/ImageCache.java
+++ trunk/Diamond/src/Diamond/ImageCache.java
@@ -4,24 +4,39 @@
  */
 package Diamond;
 
+import java.util.LinkedList;
+
 /**
  *
  * @author jusjoken
  */
 public class ImageCache {
-    public ImageCache(){
-        //a ImageCache object needs
-        // - Cache - SoftHashMap - this is the Cache itself
-        //   - MinCacheItems - keep this number of items in the Cache when Memory is cleaned
-        // - Queue - HashMap - list of MediaObjects to get a specific ImageType to add to the Cache
-        // - 
-        
-    }
+//    public ImageCache(){
+//        
+//    }
     
+    //a ImageCache object needs
+    // - Cache - SoftHashMap - this is the Cache itself
+    //   - MinCacheItems - keep this number of items in the Cache when Memory is cleaned
+    // - Queue - HashMap - list of MediaObjects to get a specific ImageType to add to the Cache
+    // - 
     //Global Functions/Variables
     // - CacheType - Background - items get added to Queue for background processing
-    // - CacheType - Immediate - items get added to the Cache and returned
-    // - CacheType - ByImageType - 
+    // - CacheType - NoQueue - items get added to the Cache and returned
     // - CacheType - Off - items get returned and NOT added to Queue NOR Cache
+    // - CacheType - ByImageType - 
+    private static final String ICacheProps = Const.BaseProp + Const.PropDivider + Const.ImageCacheProp;
+    private static LinkedList IQueue = new LinkedList();
+    private static SoftHashMap ICache = new SoftHashMap(GetMinSize());
+    
+    
+    public static Integer GetMinSize(){
+        String tProp = ICacheProps + Const.PropDivider + Const.ImageCacheMinSize;
+        return util.GetPropertyAsInteger(tProp, 100);
+    }
+    public static void SetMinSize(Integer Value){
+        String tProp = ICacheProps + Const.PropDivider + Const.ImageCacheMinSize;
+        util.SetProperty(tProp, Value.toString());
+    }
     
 }
