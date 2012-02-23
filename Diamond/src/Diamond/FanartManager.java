ae0a07202d6e0976623853e46692705f4ec4374c
usjusjoken <usjusjoken@gmail.com>
2012-02-23 01:43:51 +0000

    Cache functions - in progress - - version 4.013

--- Diamond/src/Diamond/FanartManager.java
+++ Diamond/src/Diamond/FanartManager.java
@@ -17,7 +17,7 @@ public class FanartManager {
     private IMediaResource PrimaryMediaResource = null;
     public static enum FanartManagerTypes{TV,MOVIE,NONE};
     private FanartManagerTypes FanartManagerType = FanartManagerTypes.NONE;
-    private String FanartType = "Poster";
+    private String FanartType = "poster";
     
     public FanartManager(IMediaResource MediaResource){
         this.MediaResource = MediaResource;
@@ -48,12 +48,57 @@ public class FanartManager {
         }
     }
 
+    public String getTitle() {
+        String Title = "No Fanart Found";
+        if (this.FanartManagerType.equals(FanartManagerType.MOVIE)){
+            Title = PrimaryMediaResource.getTitle();
+        }else if (this.FanartManagerType.equals(FanartManagerType.TV)){
+            Title = PrimaryMediaResource.getTitle();
+            //append the Current Season if selected
+        }
+        return Title;
+    }
+
     public String getFanartType() {
         return FanartType;
     }
 
+    public Boolean IsFanartTypePoster(){
+        return this.FanartType.toLowerCase().equals("poster");
+    }
+    public Boolean IsFanartTypeBanner(){
+        return this.FanartType.toLowerCase().equals("banner");
+    }
+    public Boolean IsFanartTypeBackground(){
+        return this.FanartType.toLowerCase().equals("background");
+    }
+
     public void setFanartType(String FanartType) {
-        this.FanartType = FanartType;
+        this.FanartType = FanartType.toLowerCase();
+    }
+
+    public Integer getTableCols() {
+        if (IsFanartTypePoster()){
+            return 4;
+        }else if (IsFanartTypeBackground()){
+            return 3;
+        }else if (IsFanartTypeBanner()){
+            return 2;
+        }else{
+            return 4; //assume poster
+        }
+    }
+
+    public Integer getTableRows() {
+        if (IsFanartTypePoster()){
+            return 2;
+        }else if (IsFanartTypeBackground()){
+            return 3;
+        }else if (IsFanartTypeBanner()){
+            return 4;
+        }else{
+            return 2; //assume poster
+        }
     }
 
     public Boolean IsTV(){
