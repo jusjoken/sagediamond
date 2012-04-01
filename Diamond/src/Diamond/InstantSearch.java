7b4211ecbadca46b1b8e7a7c24757fdab4e32ab9
usjusjoken <usjusjoken@gmail.com>
2012-04-01 01:28:09 +0000

    Instant Search in progress - working on JumptTo - version 4.019

--- Diamond/src/Diamond/InstantSearch.java
+++ Diamond/src/Diamond/InstantSearch.java
@@ -20,12 +20,12 @@ public class InstantSearch {
 
     static private final Logger LOG = Logger.getLogger(InstantSearch.class);
 
-    public static Object FilteredList(String FlowName,String SearchKeys,Object MediaFiles, Boolean IsNumericKeyListener){
-        if (FlowName==null || SearchKeys==null || SearchKeys.isEmpty()){
-            LOG.debug("FilteredList: invalid parameters so returning full list. FlowName '" + FlowName + "' SearchKeys '" + SearchKeys + "'");
+    public static Object FilteredList(String SearchKeys,Object MediaFiles, Boolean IsNumericKeyListener){
+        if (SearchKeys==null || SearchKeys.isEmpty()){
+            LOG.debug("FilteredList: invalid parameters so returning full list. SearchKeys '" + SearchKeys + "'");
             return MediaFiles;
         }
-        StopWatch Elapsed = new StopWatch("Flitering " + FlowName + " by '" + SearchKeys + '"');
+        StopWatch Elapsed = new StopWatch("Flitering by '" + SearchKeys + '"');
         Elapsed.Start();
         Object[] InputMediaFiles = FanartCaching.toArray(MediaFiles);
         //LOG.debug("FilteredList: InputMediaFiles = '" + InputMediaFiles.length);
@@ -44,8 +44,85 @@ public class InstantSearch {
         if (sagex.api.Utility.Size(OutputMediaFiles)>0){
             return OutputMediaFiles;
         }
-        LOG.debug("FilteredList: empty search result so returning full list. FlowName '" + FlowName + "' SearchKeys '" + SearchKeys + "'");
+        LOG.debug("FilteredList: empty search result so returning full list. SearchKeys '" + SearchKeys + "'");
         return MediaFiles;
+    }
+
+    public static Object JumpTo(Object MediaFiles, String SearchKey, String LastSearchKey, Boolean IsNumericKeyListener, Boolean AlphaJump){
+        if (MediaFiles==null){
+            LOG.debug("JumpTo: null MediaFiles so returning null. SearchKey '" + SearchKey + "' LastSearchKey '" + LastSearchKey + "'");
+            return null;
+        }
+        Object[] InputMediaFiles = FanartCaching.toArray(MediaFiles);
+        if (SearchKey==null || SearchKey.isEmpty()){
+            LOG.debug("JumpTo: invalid SearchKey so returning first item. SearchKey '" + SearchKey + "' LastSearchKey '" + LastSearchKey + "'");
+            if (InputMediaFiles.length>0){
+                LOG.debug("JumpTo: invalid SearchKey so returning first item. SearchKey '" + SearchKey + "' LastSearchKey '" + LastSearchKey + "'");
+                return InputMediaFiles[0];
+            }else{
+                LOG.debug("JumpTo: invalid SearchKey and no items so returning null. SearchKey '" + SearchKey + "' LastSearchKey '" + LastSearchKey + "'");
+                return null;
+            }
+        }
+        StopWatch Elapsed = new StopWatch("JumpTo SearchKey '" + SearchKey + "' LastSearchKey '" + LastSearchKey + "'");
+        Elapsed.Start();
+        //LOG.debug("FilteredList: InputMediaFiles = '" + InputMediaFiles.length);
+        if (AlphaJump){  //convert SearchKey to a Alpha
+            //TODO: Convert SearchKey to an Alpha and return the first matching item
+            LOG.debug("JumpTo: Alpha jump - not written. SearchKey '" + SearchKey + "' LastSearchKey '" + LastSearchKey + "'");
+        }else{  //convert SearchKey to a percent
+            Integer Element = 0;
+            Integer Key = 10;
+            String Alphabet = "abcdefghijklmnopqrstuvwxyz";
+            try {
+                Key = Integer.valueOf(SearchKey);
+            } catch (NumberFormatException ex) {
+                //use the first item by default
+            }
+            LOG.debug("JumpTo: Key = '" + Key + " SearchKey '" + SearchKey + "' LastSearchKey '" + LastSearchKey + "'");
+            if (Key.equals(10)){
+                if (IsNumericKeyListener){
+                    LOG.debug("JumpTo: Here 1");
+                    Element = 0;
+                }else{
+                    Integer Loc = Alphabet.indexOf(SearchKey.toLowerCase());
+                    LOG.debug("JumpTo: Here 2 Loc '" + Loc + "'");
+                    if (Loc.equals(-1)){
+                        LOG.debug("JumpTo: Here 3");
+                        Element = 0;
+                    }else{
+                        Element = (((Loc+1)*100/26)*InputMediaFiles.length/100)-1;
+                        LOG.debug("JumpTo: Here 4 Element '" + Element + "'");
+                    }
+                }
+            }else if (Key.equals(0)){
+                LOG.debug("JumpTo: Here 5");
+                Element = InputMediaFiles.length - 1;
+            }else{
+                LOG.debug("JumpTo: Here 6");
+                Element = ((Key*100/10)*InputMediaFiles.length/100)-1;
+            }
+            Elapsed.StopandLog();
+            if (Element>InputMediaFiles.length){
+                Element = InputMediaFiles.length-1;
+            }
+            LOG.debug("JumpTo: Element '" + Element + "' SearchKey '" + SearchKey + "' LastSearchKey '" + LastSearchKey + "' Item '" + InputMediaFiles[Element] + "'");
+            return InputMediaFiles[Element];
+        }
+//        Object OutputMediaFiles = null;
+//        if (IsNumericKeyListener){
+//            SearchKeys=CreateRegexFromKeypad(SearchKeys);
+//        }
+//        Pattern SearchPattern = Pattern.compile(SearchKeys);
+//        OutputMediaFiles = sagex.api.Database.FilterByMethodRegex(InputMediaFiles, "Diamond_MetadataCalls_GetTitleLowerCase", SearchPattern, true, false);
+//        //remove these 2 lines after testing
+//        //Object[] Tempfiles=FanartCaching.toArray(OutputMediaFiles);
+//        //LOG.debug("FilteredList: OutputMediaFiles = '" + Tempfiles.length);
+//        //remove these 2 lines above after testing
+//        //LOG.debug("InstantSearch using RegEx = '" + SearchKeys + "'");
+        Elapsed.StopandLog();
+        LOG.debug("JumpTo: no item found so returning first item. SearchKey '" + SearchKey + "' LastSearchKey '" + LastSearchKey + "'");
+        return InputMediaFiles[0];
     }
 
     public static String AddKey(String SearchString, String AddedString, Boolean IsNumericKeyListener){
